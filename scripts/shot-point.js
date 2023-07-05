3497675a8dcef871037b0f9c22a214e2a1c24901
AnnKomissarova <nnkmssrv@gmail.com>
2023-07-05 17:02:14 +0300

    addig-download-buttons

--- scripts/shot-point.js
+++ scripts/shot-point.js
@@ -1,10 +1,44 @@
 const block = document.querySelector('.block');
-var point = document.createElement('div');
+const bigBlock = document.querySelector('.fullscreen-block');
+const image = document.querySelector('.image');
 
-block.onclick = function(e) {
-  var point = document.createElement('div');
-  point.className = 'point';
-  point.style.left = e.pageX + 'px';
-  point.style.top = e.pageY + 'px';
-  block.appendChild(point);
+// block.onclick = function (e) {
+//   this.requestFullscreen();
+
+//   bigBlock.onclick = function(e) {
+//     var point = document.createElement('div');
+//     point.className = 'point';
+//     point.style.left = e.clientX - 6 + 'px';
+//     point.style.top = e.clientY - 4 + 'px';
+//     bigBlock.appendChild(point);
+//   }  
+// }
+
+document.oncontextmenu = function (e) { return false };
+
+block.onclick = function (e) { this.requestFullscreen(); 
+
+  function addRedPoint(e) {
+    var point = document.createElement('div');
+    point.className = 'point-red';
+    point.style.left = e.clientX - 6 + 'px';
+    point.style.top = e.clientY - 4 + 'px';
+    bigBlock.appendChild(point);
+  }
+  
+  function addGreenPoint(e) {
+    var point = document.createElement('div');
+    point.className = 'point-green';
+    point.style.left = e.clientX - 6 + 'px';
+    point.style.top = e.clientY - 4 + 'px';
+    bigBlock.appendChild(point);
+  }
+
+  bigBlock.addEventListener('click', addRedPoint);
+  bigBlock.addEventListener('contextmenu', addGreenPoint);
+
 }
+
+
+
+
