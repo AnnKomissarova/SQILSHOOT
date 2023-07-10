f3bcd7cd0cd19725959cdd16301d626114ce7f11
AnnKomissarova <nnkmssrv@gmail.com>
2023-07-10 14:08:28 +0300

    Adding-shooting-modal

--- scripts/shot-point.js
+++ scripts/shot-point.js
@@ -1,44 +1,98 @@
-const block = document.querySelector('.block');
-const bigBlock = document.querySelector('.fullscreen-block');
-const image = document.querySelector('.image');
+var myImage = document.getElementById("myImage");
+var modal = document.getElementById("myModal");
+var modalImage = document.getElementById("modalImage");
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
+var removeButton = document.getElementById("removeButton");
+var exitButton = document.getElementById("exitButton");
+var points = [];
+var scale = 1;
+
+myImage.onclick = function () {
+  modal.style.display = "block";
+  modalImage.src = this.src;
+  modalImage.onload = function () {
+    canvas.width = modalImage.width;
+    canvas.height = modalImage.height;
+    canvas.style.top = modalImage.offsetTop + 'px';
+    canvas.style.left = modalImage.offsetLeft + 'px';
+  };
+
+  points = [];
+
+  drawPoints();
+}
+
+canvas.onclick = function (e) {
+  e.stopPropagation();
+}
+
+canvas.oncontextmenu = function (e) {
+  e.preventDefault();
+}
+
+removeButton.onclick = function () {
+  if (points.length > 0) {
+    points.pop();
+    drawPoints();
+  }
+}
+
+canvas.onmousedown = function (e) {
+  e.preventDefault();
+
+  var p_color = '';
+
+  if (e.button === 0) {
+    p_color = "green";
+  } else if (e.button === 2) {
+    p_color = "red";
+  }
+
+  context.fillStyle = p_color;
+
+  var x = e.clientX - canvas.offsetLeft;
+  var y = e.clientY - canvas.offsetTop;
+  points.push({ x: x, y: y, color: p_color });
+  // context.beginPath();
+  // context.arc(x, y, 4.5, 0, 2 * Math.PI);
+  // context.fill();
+
+  drawPoints();
+}
+
+window.onkeydown = function (e) {
+  if (e.key === "Escape") {
+    modal.style.display = "none";
+  }
+}
+
+function drawPoints() {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  for (var i = 0; i < points.length; i++) {
+    var point = points[i];
+    context.fillStyle = point.color;
+    context.beginPath();
+    context.arc(point.x, point.y, 4.5, 0, 2 * Math.PI);
+    context.fill();
+  }
+}
+
+function closeModal() {
+  modal.style.display = "none";
+};
+
+exitButton.addEventListener('click', closeModal);
 
 // block.onclick = function (e) {
 //   this.requestFullscreen();
-
 //   bigBlock.onclick = function(e) {
 //     var point = document.createElement('div');
 //     point.className = 'point';
 //     point.style.left = e.clientX - 6 + 'px';
 //     point.style.top = e.clientY - 4 + 'px';
 //     bigBlock.appendChild(point);
-//   }  
+//   }
 // }
-
-document.oncontextmenu = function (e) { return false };
-
-block.onclick = function (e) { this.requestFullscreen(); 
-
-  function addRedPoint(e) {
-    var point = document.createElement('div');
-    point.className = 'point-red';
-    point.style.left = e.clientX - 6 + 'px';
-    point.style.top = e.clientY - 4 + 'px';
-    bigBlock.appendChild(point);
-  }
-  
-  function addGreenPoint(e) {
-    var point = document.createElement('div');
-    point.className = 'point-green';
-    point.style.left = e.clientX - 6 + 'px';
-    point.style.top = e.clientY - 4 + 'px';
-    bigBlock.appendChild(point);
-  }
-
-  bigBlock.addEventListener('click', addRedPoint);
-  bigBlock.addEventListener('contextmenu', addGreenPoint);
-
-}
-
-
 
 
